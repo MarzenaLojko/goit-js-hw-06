fd59feb03915b6f01af9021f2cca63dd3f056bef
Piotr Łojko <80154448+S19514@users.noreply.github.com>
2022-09-23 00:28:39 +0200

    update.t2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,32 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+const listIngrediens = document.querySelector("#ingredients")
+
+
+const newElements = ingredients.map((ingredient) => {
+const element = document.createElement("li");
+element.textContent = ingredient.toString();
+console.log('tst' + element.textContent)
+element.classList.add( ingredient);
+console.log(element.classList);
+});
+
+
+listIngrediens.append(...newElements);
+
+
+
+
+const ingredientsList = document.getElementById('ingredients');
+
+for (let ingredient of ingredients) {
+  const ingredientEl = document.createElement('li');
+  ingredientEl.innerText = ingredient;
+  ingredientEl.classList.add("item");
+  ingredientsList.appendChild(ingredientEl);
+}
+
+
+
+
