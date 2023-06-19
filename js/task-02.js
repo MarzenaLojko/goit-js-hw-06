c48f31b1b202988f772b28b30fee746c7c254237
Piotr Łojko <80154448+S19514@users.noreply.github.com>
2023-06-19 21:55:17 +0200

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -6,11 +6,24 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
-const ingredientsList = document.getElementById('ingredients');
-
-for (let ingredient of ingredients) {
-  const ingredientElement = document.createElement('li');
-  ingredientElement.innerText = ingredient;
-  ingredientElement.classList.add('item');
-  ingredientsList.appendChild(ingredientElement);
+const ingredientList = document.getElementById('ingredients');
+const ingredientElement = [];
+for (let i = 0; i < ingredients.length; i++) {
+  ingredientElement[i] = document.createElement('li');
+  ingredientElement[i].textContent = ingredients[i];
+  ingredientElement[i].classList.add('item');
 }
+ingredientList.append(...ingredientElement);
+// wewnątrz pętli powoduje,
+//  że operacja dodawania elementów
+//  do html'a wykonuje się tyle razy,
+//  ile mamy elementów.
+//  możemy jednak nie używać appendChild wewnątrz pętli .
+//  Mamy metodę .append, która pozwala w taki sposób
+//  domNode.append(...tablicaElementow)
+//  dodać wszystkie elementy znajdujące się w
+//  tablicy podczas jednej operacji.
+//  Trzeba Ci zmienić kod w taki sposób,
+//  by wewnątrz forEach tylko zapisywać
+//  elementy do jakiejś tablicy,
+//  a już dopiero po pętli robić ten .append.
