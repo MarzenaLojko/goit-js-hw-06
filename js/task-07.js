523f17fc01b11a2389d149dc023143fb9edef6ca
Piotr Łojko <80154448+S19514@users.noreply.github.com>
2023-06-19 22:02:03 +0200

    Update task-07.js

--- js/task-07.js
+++ js/task-07.js
@@ -1,4 +1,4 @@
-const myInput = document.querySelector('#font-event-control');
+const myInput = document.querySelector('#font-size-control');
 const text = document.querySelector('#text');
 const SizeInput = (event) => {
   text.style.fontSize = `${event.target.value}px`;
